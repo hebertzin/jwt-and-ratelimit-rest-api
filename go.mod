888fda3718c053a959331bdc417fbef641d58e4c
Hebert santos <hebertsantosdeveloper@gmail.com>
2025-08-28 14:57:45 -0300

    feat: create users repository

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jwt-and-ratelimit-rest-api
+
+go 1.24.5
---
+++ src/infra/repository/users_repository.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/jwt-and-ratelimit-rest-api/src/domain"
+)
+
+type UsersRepository interface {
+	Create(ctx context.Context, user domain.User) (int64, error)
+	FindByEmail(ctx context.Context, email string) (domain.User, error)
+}
+
+type UserPostgresRepository struct {
+	DB *sql.DB
+}
+
+func NewUsersRepository(db *sql.DB) UsersRepository {
+	return &UserPostgresRepository{
+		DB: db,
+	}
+}
+
+func (ur *UserPostgresRepository) Create(ctx context.Context, user domain.User) (int64, error) {
+	q := `INSERT INTO users (name, email, password) 
+          VALUES ($1, $2, $3) 
+          RETURNING id`
+
+	var id int64
+	tx := ur.DB.QueryRowContext(ctx, q, user.Name, user.Email, user.Password)
+	tx.Scan(&id)
+	if tx != nil {
+		return 0, nil
+	}
+
+	return id, nil
+}
+
+func (ur *UserPostgresRepository) FindByEmail(ctx context.Context, email string) (domain.User, error) {
+	u := domain.User{}
+	q := `SELECT * FROM users WHERE email = ?`
+	tx := ur.DB.QueryRowContext(ctx, q, email)
+	tx.Scan(&u.Name, &u.Email, &u.Password)
+	return domain.User{}, tx.Err()
+}
