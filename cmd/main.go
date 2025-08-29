f7b1722abe087f24d1d9b5bc28929825340ce0c4
Hebert santos <hebertsantosdeveloper@gmail.com>
2025-08-29 11:43:27 -0300

    feat: connect in database and server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"context"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	routing "github.com/jwt-and-ratelimit-rest-api/src/router"
+)
+
 func main() {
-	print("hello world")
+
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatal("error connecting database", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatal("cannot connect to postgres", err)
+	}
+
+	r := chi.NewRouter()
+
+	routing.UsersGoupRouter(r, db)
+
+	http.ListenAndServe(":8080", r)
 }
