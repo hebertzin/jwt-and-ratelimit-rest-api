5aeafb2b6bc76a3a6f5646b1e050b1c7d12e3b3b
Hebert santos <hebertsantosdeveloper@gmail.com>
2025-09-03 12:08:29 -0300

    refactor: using users factory

--- src/router/users_router.go
+++ src/router/users_router.go
@@ -4,26 +4,14 @@ import (
 	"database/sql"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/jwt-and-ratelimit-rest-api/src/handler"
-	"github.com/jwt-and-ratelimit-rest-api/src/infra/repository"
-	"github.com/jwt-and-ratelimit-rest-api/src/infra/security"
+	"github.com/jwt-and-ratelimit-rest-api/src/infra/factory"
 	"github.com/jwt-and-ratelimit-rest-api/src/middlewares"
-	"github.com/jwt-and-ratelimit-rest-api/src/services"
-	"github.com/jwt-and-ratelimit-rest-api/src/utils/validation"
 )
 
 func UsersGoupRouter(r chi.Router, db *sql.DB) {
-	userRepository := repository.NewUsersRepository(db)
-
-	hasher := security.NewBcryptHasher(10)
-
-	pv := validation.NewPayloadValidate()
-
-	userService := services.NewUserService(userRepository, hasher, pv)
-
-	userHandler := handler.NewUserHandler(userService)
+	u := factory.UsersFactory(db)
 
 	r.Use(middlewares.DoFilter)
 
-	r.Post("/api/v1/accounts", userHandler.Create)
+	r.Post("/api/v1/accounts", u.Create)
 }
